c382ca8c8318271d76e6882d5839c4448797ade1
gustavo-luz <gustavo.luz.unb@gmail.com>
2021-03-23 19:19:38 -0300

    start_project4

---
+++ dolar_2_brl (copy)/app.js
@@ -0,0 +1,10 @@
+/*alert("alerta js")*/
+
+var valoremdolfloat = parseFloat(prompt("valor em dólar pra converter?"))
+//var valoremdolfloat = parseFloat(valoremdol)
+
+var valoremreal = (valoremdolfloat * 5.50).toFixed(2)
+
+alert(valoremreal + " reais")
+
+// comentário tambem
